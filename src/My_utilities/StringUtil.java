3f795654a77e1db48b0467cd9397c429f6ab3929
burak0arslan <burakarslan42@icloud.com>
2022-04-10 12:23:55 -0400

    constractor

--- src/My_utilities/StringUtil.java
+++ src/My_utilities/StringUtil.java
@@ -1,5 +1,7 @@
 package My_utilities;
 
+import java.util.Locale;
+
 public class StringUtil {
 
     public static String reverse(String str){
@@ -11,7 +13,48 @@ public class StringUtil {
         return reversed;
     }
 
-    public static void main(String[] args) {
-        System.out.println(reverse("java"));
+    /*
+    Fix Format
+create a method will accept a String and return a String in proper format. Proper format is:
+First character starting as uppercase and the rest as lowercase
+     */
+
+    public static String fixFormat(String str){
+
+        str = str.trim();
+        return str.substring(0,1).toUpperCase(Locale.ROOT)+str.substring(1).toLowerCase(Locale.ROOT);
+    }
+
+    public static int frequencyOfCharacter(String str, char letter){
+
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == letter){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static String uniqueCharacters(String str){
+        String unique = "";
+        String checked = "";
+
+        for (int i = 0 ; i < str.length() ; i++){
+            if (!checked.contains(""+ str.charAt(i))){
+                int count = StringUtil.frequencyOfCharacter(str,str.charAt(i));
+                checked+= str.charAt(i);
+
+                if (count == 1 ){
+                    unique += str.charAt(i);
+                }
+            }
+        }
+        return unique;
+
     }
 }
+
+
---
+++ src/day43_custom_classes/TrafficLight.java
@@ -0,0 +1,11 @@
+package day43_custom_classes;
+
+public class TrafficLight {
+
+    String color;
+
+    public TrafficLight(String startColor){     //startColor is ==> local variable for constractor (parameter)
+        color = startColor;     //color is ==> instance variable
+    }
+
+}
